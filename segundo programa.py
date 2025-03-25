2223142b28b4b3bcc452006e682203e3ec324e8d
vinihoss <vinihoss@gmail.com>
2025-03-24 22:48:02 -0300

    segunda aula

--- segundo programa.py
+++ segundo programa.py
@@ -1,2 +1,24 @@
 d = True
 print(type(d))
+
+
+#print(10*3)
+#print(10//3)
+#print(10%3)
+
+
+nota = 8
+media = 7
+aprovado = nota >= media
+print("aprovado")
+
+
+nome_completo = input("Nome completo: ")
+idade = int(input("Idade: "))
+altura = float(input("Altura: "))
+
+
+print(f"Nome completo: {nome_completo}")
+print(f"Idade: {idade}")
+print("Altura:", altura)
+
