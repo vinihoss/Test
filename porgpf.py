6287ae45b874617cb129446b058f48f449752dc3
vinicius <vinihoss@gmail.com>
2025-08-04 21:53:18 -0300

    Primeiro prog pos ferias

---
+++ porgpf.py
@@ -0,0 +1,13 @@
+carros = []
+print(carros)
+print(len(carros))
+
+carros = ["Accord","Polo", "Civic", "Fiat Uno"]
+print(carros)
+print(len(carros))
+#Indice começa no 0; e tratado com []
+print(carros[1])
+print(carros[3])
+carros[0] = "Jetta" #Trocar o nome caso tenha errado
+print(carros[0])
+
