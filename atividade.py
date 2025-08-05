53618823ebe562cc0af303d296600ff0f9d53aa0
vinicius <vinihoss@gmail.com>
2025-08-04 22:14:01 -0300

    ativ

--- atividade.py
+++ atividade.py
@@ -1,7 +1,17 @@
-notas = [6, 7, 8, 9]
+#notas = [6, 7, 8, 9]
+#soma = 0
+#x = 0
+#while x<5:
+ #   soma +=notas(x)
+  #  x = 1
+#print(f"Média ={(soma/x):.2f}")
+
+#Outra maneira
+notas = {0,0,0,0,0}
 soma = 0
 x = 0
-while x<5:
-    soma +=notas(x)
-    x = 1
-print(f"Média ={(soma/x):.2f}")
+while x < 5:
+ notas[x] = float(input(f"Nota{x=1} ="))
+ soma += notas[x]
+ x += 1
+ print((f"Média ={(soma/x):.2f}")
